1af3d49d68c27436e4923cf661a7c813910168cc
markparagas <mark.paragas@student.ehb.be>
2021-12-14 15:45:27 +0100

    First commit
    Html & Css
    Reset.css
    Icon fonts
    
    add Arrow animation
    
    Get Data from API

---
+++ README.md
@@ -0,0 +1,6 @@
+# web-2-project-frontend-mark
+
+Used source:
+- API Tea: https://tea-api-vic-lo.herokuapp.com/
+- Font icons: https://fontawesome.com/
+- Scroll animate arrows: https://codepen.io/raf187/pen/BvgGRQ
---
+++ js/frontend.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// ------------------- WIP ------------------- 
+let html = '';
+
+window.onload = () => {
+    fetch('https://tea-api-vic-lo.herokuapp.com/tea')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Getting data');
+            console.log(data);
+
+            data.forEach(data => {
+
+                html += `
+                <section>
+                    <div class="image_wrapper">
+                        <img src="./${data.image}">
+                    </div>
+                    <div class="article_content_wrapper">
+                        <div>
+                            <h2>${data.name}</h2>
+                        </div>
+                        <div class="content">
+                            ${data.description}
+                        </div>
+                    </div>
+                </section>
+                `;
+
+            })
+
+            document.getElementById("container").innerHTML = html;
+
+        });
+}
