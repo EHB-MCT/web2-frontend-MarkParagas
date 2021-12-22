9196566ca5a4bd6b294569a38552ce8418f3fe62
MarkParagas <mark.paragas@student.ehb.be>
2021-12-22 15:41:42 +0100

    Update structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+"use strict";
+
+// ------------------- WIP ------------------- 
+
+let html = '';
+
+window.onload = () => {
+    fetch('https://tea-api-vic-lo.herokuapp.com/tea')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Getting data');
+            console.log(data);
+
+            data.forEach(data => {
+
+                html += `
+                <section>
+                    <div class="image_wrapper">
+                        <img src="./${data.image}">
+                    </div>
+                    <div class="article_content_wrapper">
+                        <div>
+                            <h2>${data.name}</h2>
+                        </div>
+                        <div class="content">
+                            ${data.description}
+                        </div>
+                    </div>
+                </section>
+                `;
+
+            })
+
+            document.getElementById("container").innerHTML = html;
+
+        });
+}
